cddc1c0205823b84a8a60548992987b915c7b4fe
Laercio Silva <laercio.sl@hotmail.com>
2021-04-22 17:55:06 -0300

    feat: register settings in BD and create file of routes

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,13 @@ import express from "express";
 
 import "./database";
 
+import { routes } from "./routes"
+
 const app = express();
+
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3334, () => {
   console.log("Server ir running on port 3334")
